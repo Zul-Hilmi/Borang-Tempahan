5c358a076d5560d639b9149bc439a8b52e311efa
Zul Hilmi <2021011271@student.uitm.edu.my>
2023-06-12 11:32:03 +0800

    dsdsd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
   integrations: [tailwind(), solidJs()]
 });
