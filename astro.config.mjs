a62c7246d65941efab7e0bb914a72964f38a921d
Zul Hilmi <2021011271@student.uitm.edu.my>
2023-06-14 08:55:07 +0800

    fi

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
